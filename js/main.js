c2e7a9f17afd509b5bdef3197a67cd31b2beaa5f
stepan323446 <stevedekart2020@gmail.com>
2022-11-05 22:20:47 +0100

    .

---
+++ js/main.js
@@ -0,0 +1,33 @@
+let listItems = [
+    {
+        title: "Very first anime you watched",
+        link: "https://anilist.co/activity/472252544",
+        image: "images/challenge/1.jpg"
+    },
+    {
+        title: "Favorite anime you've watched so far",
+        link: "https://anilist.co/activity/472554642",
+        image: "images/challenge/2.jpg"
+    },
+    {
+        title: "Favorite male anime character ever",
+        link: "https://anilist.co/activity/472952368",
+        image: "images/challenge/3.png"
+    }
+]
+
+const itemsContainer = document.getElementById("challenge-items");
+
+for(let i = 0; i < listItems.length; i++){
+    itemsContainer.innerHTML += `
+                <a href="${listItems[i].link}" target="_blank" class="item">
+                    <div class="item-img">
+                        <img src="${listItems[i].image}" alt="">
+                    </div>
+                    <div class="info">
+                        <h3>${listItems[i].title}</h3>
+                    </div>
+                    <span>${i + 1} Day</span>
+                </a>
+    `;
+}
