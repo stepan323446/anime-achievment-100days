6362c48af525d150553f5212428c96d8c4de5a0e
stepan323446 <stevedekart2020@gmail.com>
2022-11-20 21:11:53 +0100

    18 Day

--- js/main.js
+++ js/main.js
@@ -94,15 +94,25 @@ let listItems = [
     }
 ]
 
+
 const itemsContainer = document.getElementById("challenge-items");
+let items = [];
+let hrBlocks = [];
 
 for(let i = 0; i < listItems.length; i++){
     if((i % 10) == 0 && i != 0){
-        itemsContainer.innerHTML += `<div class="hr-block"><div>${i}+</div></div>`;
+        let hrBlock = document.createElement("div");
+        hrBlock.classList.add("hr-block");
+        hrBlock.innerHTML = `<div>${i}+</div>`;
+
+        hrBlocks.push(hrBlock);
+        itemsContainer.append(hrBlock);
     }
 
-    itemsContainer.innerHTML += `
-                <a href="${listItems[i].link}" class="item">
+    let item = document.createElement("a");
+    item.classList.add("item");
+    item.setAttribute("href", listItems[i].link);
+    item.innerHTML = `
                     <div class="item-img">
                         <img src="${listItems[i].image}" alt="">
                     </div>
@@ -110,6 +120,116 @@ for(let i = 0; i < listItems.length; i++){
                         <h3>${listItems[i].title}</h3>
                     </div>
                     <span>${i + 1} Day</span>
-                </a>
-    `;   
+    `
+    items.push({
+        elem: item,
+        searchText: `${listItems[i].title.toLowerCase()} - ${i + 1} day`,
+        day: i + 1
+    });
+    itemsContainer.append(item);
+}
+
+// Поиск в реальном времени
+let searchInput = document.getElementById("search-input");
+searchInput.addEventListener("input", function(e){
+
+    // Отключить кнопки-сортировки
+    daysSelectBtns.forEach(btn => {
+        btn.classList.remove("selected");
+    });
+    daysSelectBtns[0].classList.add("selected");
+
+    let value = e.currentTarget.value.toLowerCase();
+    if(value == "")
+    {
+        items.forEach(element => {
+            element.elem.classList.remove("hide");
+        });
+        isActiveHrBlocks(true);
+        isActiveNothing(false);
+        return;
+    }
+    isActiveHrBlocks(false);
+
+    let founded = 0;
+    items.forEach(element => {
+        if(element.searchText.includes(value)){
+            element.elem.classList.remove("hide");
+            founded++;
+        }
+        else{
+            element.elem.classList.add("hide");
+        }
+    });
+    if(founded == 0)
+        isActiveNothing(true);
+    else
+        isActiveNothing(false);
+});
+
+
+// Кнопки-сортировки по дням
+let daysSelectBtns = document.querySelectorAll("#select-days .select");
+daysSelectBtns.forEach(btn => {
+    btn.addEventListener("click", function(e){
+        let elem = e.currentTarget;
+
+        // Если кнопка выбрана, то она активна и выделяется визуальна
+        daysSelectBtns.forEach(btn => {
+            btn.classList.remove("selected");
+        });
+        elem.classList.add("selected");
+
+
+        // Получение от минимального числа дня до максимального
+        let min = +elem.getAttribute("min-day");
+        let max = +elem.getAttribute("max-day");
+
+        let founded = 0;
+        // Поиск тех дней, что нужны
+        items.forEach(item => {
+            if(min <= item.day && item.day <= max)
+            {
+                item.elem.classList.remove("hide");
+                founded++;
+            }
+            else{
+                item.elem.classList.add("hide");
+            }
+            // Если выбраны все дни, то возвращаем разделительные блоки, как и было раньше
+            if(min == 1 && max == 100)
+                isActiveHrBlocks(true);
+            else
+                isActiveHrBlocks(false);
+        });
+        if(founded == 0)
+            isActiveNothing(true);
+        else
+            isActiveNothing(false);
+    });
+});
+
+// Включить / Выключить разделительные блоки
+function isActiveHrBlocks(isActive){
+    if(isActive){
+        hrBlocks.forEach(element => {
+            element.classList.remove("hide");
+        });
+    }
+    else{
+        hrBlocks.forEach(element => {
+            element.classList.add("hide");
+        });
+    }
+    
+}
+
+// Активировать текст, если ничего не найдено
+let nothing = document.getElementById("nothing-text");
+nothing.textContent = `Unfortunately we didn't find anything. Most likely, you entered an invalid request or I have not completed this task yet (the maximum completed day is ${items.length})`
+function isActiveNothing(isActive){
+    if(isActive)
+        nothing.classList.remove("hide");
+    else
+        nothing.classList.add("hide");
 }
